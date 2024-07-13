b0fd9d63e23a7c223fdfe2921d34b589f1ec6bab
Seajal gupta <113388188+Seajal2004@users.noreply.github.com>
2024-07-13 18:57:38 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -1,4 +1,4 @@
-#Binary Search
+## Binary Search
 #Ques:1 You are given an integer array 'A' of size 'N', sorted in non-decreasing order. You are also given an integer 'target'. Your task is to write a function to search for 'target' in the array 'A'. If it exists, return its index in 0-based indexing. If 'target' is not present in the array 'A', return -1.
 
 int search(vector<int> &nums, int target) {
